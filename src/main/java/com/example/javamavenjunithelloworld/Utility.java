e4f2e27dffbf5e04b8ef064d437268cf2dfe8ded
Nagesh <kassaambadas40@gmail.com>
2023-09-30 19:26:49 +0530

    NAGESHK0001-Wrote the code for finding the add 2 numbers

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,15 @@
+public class Utility {
+    public static void nagesh(){
+        System.out.println("Hello I am Nagesh Kassa");
+    }
+    public static int addition(int a,int b){
+        return a+b;
+
+    }
+    public static void main(String[] args) {
+        nagesh();
+        System.out.println("Sum of two numbers is "+addition(5,6));
+
+    }
+    
+}
