003e0196d22b4929dd98f81ba37baf4ea35a3608
harsha <bayyaramharsha@gmail.com>
2023-10-03 20:09:16 +0530

    added print function

--- src/main/java/com/example/javamavenjunithelloworld/Utility.java
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -3,6 +3,9 @@ package com.example.javamavenjunithelloworld;
 import java.util.Scanner;
 
 public class Utility {
+    public static void harshathkumar(){
+        System.out.println("Hello I am Harshath kumar");
+    }
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
@@ -10,7 +13,8 @@ public class Utility {
             System.out.println("EVEN NUMBER");
         } else {
             System.out.println("ODD NUMBER");
-        }
+        }   
         sc.close();
+        harshathkumar();
     }
 }
