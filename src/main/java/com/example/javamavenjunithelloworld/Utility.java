358348e0ed8b6924e50ca21c643f1b5414f2183a
mani2002 <manikantavinjamuri8522@gmail.com>
2023-10-04 13:23:59 +0530

    manikantav0001-wrote program for area of square

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,12 @@
+package com.example.javamavenjunithelloworld;
+
+public class Utility(){
+    public static void print(){
+        System.out.println("Hello I am Manikanta");
+    }
+    public static void main(String args[]){
+        int side = 5;
+        int area = side*side;
+        System.out.println(area);
+    }
+}
