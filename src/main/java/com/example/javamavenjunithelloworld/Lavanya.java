0c17b82a5e17bc8374050beb268b8be0e694841c
Lavanya <dlavanyaindapure@gmail.com>
2023-10-18 17:59:23 +0530

    LAVANYAI0001 - Created HelloWorld Program

--- src/main/java/com/example/javamavenjunithelloworld/Lavanya.java
+++ src/main/java/com/example/javamavenjunithelloworld/Lavanya.java
@@ -3,8 +3,8 @@ package com.example.javamavenjunithelloworld;
 public class Lavanya {
 
     public static void main(String[] args) {
-        
-        System.out.println("Hello");
+
+        System.out.println("Hello World");
     }
 
 }
