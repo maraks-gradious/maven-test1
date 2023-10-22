b2fc63d685fa272766603e756a05640a185357a1
Kartavya Jain <kartavya2552@gmail.com>
2023-10-22 14:58:30 +0530

    KRTAVYAJ0001 - Wrote the code for finding the area of the circle

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,9 @@
+public class Utility {
+    public void kartavya() {
+        System.out.println("I am Kartavya");
+    }
+
+    public double calculateCircleArea(double radius) {
+        return Math.PI * radius * radius;
+    }
+}
