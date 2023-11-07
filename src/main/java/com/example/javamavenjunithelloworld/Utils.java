1b738e4e5d623b031da09894c9f9a9bea2f4a870
NITHIN2109 <110482153+NITHIN2109@users.noreply.github.com>
2023-11-07 10:18:39 +0530

    Nithin0001 - Wrote code for finding the given number is even or odd

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utils.java
@@ -0,0 +1,17 @@
+public class Utils
+ {
+ public static int Nithin(int n){
+   if(n%2==0){
+System.out.println("Even");
+}
+else
+{
+System.out.println("Odd");
+  }
+}
+
+public static void main(String[] args) {
+Nithin(10);
+    
+    }
+}
