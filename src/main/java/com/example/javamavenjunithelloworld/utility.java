c3ca58d954487981c8fcae5283e2e619a05f97ce
THANGELLA BHANU PRAKASH REDDY <bhanuprakash9821@gmail.com>
2023-11-11 11:40:53 +0530

    features/bhanu001-wrote the code for multiplying 2 numbers

---
+++ src/main/java/com/example/javamavenjunithelloworld/utility.java
@@ -0,0 +1,12 @@
+package com.example.javamavenjunithelloworld;
+
+public class utility {
+
+    public static int multiply(int num1,int num2){
+        return num1*num2;
+    }
+     public static void main(String[] args) {
+        int output = multiply(10, 5);
+        System.out.println("The output is:"+output);
+     }
+}
