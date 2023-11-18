1d08363a207ebd65c829a7b9e07198716ad10c45
VaishnaviYamsani <113632173+VaishnaviYamsani@users.noreply.github.com>
2023-11-18 12:34:35 +0530

    Update Utility.java

--- src/main/java/com/example/javamavenjunithelloworld/Utility.java
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -1,7 +1,6 @@
 public class LeapYearChecker {
     public static void main(String[] args) {
-        int year = 2024; // Replace with the year you want to check
-
+        int year = 2024; 
         if (isLeapYear(year)) {
             System.out.println(year + " is a leap year.");
         } else {
@@ -10,7 +9,6 @@ public class LeapYearChecker {
     }
 
     public static boolean isLeapYear(int year) {
-        // Leap year condition: divisible by 4 but not by 100, except if it's divisible by 400
         return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
     }
 }
